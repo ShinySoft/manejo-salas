c9423e6100dc8a4a63d9eec04c2ce790b51f694b
judgonzalezmu <judgonzalezmu@unal.edu.co>
2020-10-23 12:45:30 -0500

    Creacion del entorno del proyecto

---
+++ manejo-salas/src/main/java/com/example/manejosalas/ManejoSalasApplication.java
@@ -0,0 +1,14 @@
+package com.example.manejosalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManejoSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManejoSalasApplication.class, args);
+		
+	}
+
+}
---
+++ manejo-salas/src/test/java/com/example/manejosalas/ManejoSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manejosalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManejoSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
