3e3b51bccba05f470ae95246f7a4e20bddcd0895
B612 <pmoros@unal.edu.co>
2020-11-03 17:50:49 -0500

    borrador CRUD usuarios

--- manejo-salas/src/main/java/com/example/manejosalas/controlador/UsuarioControlador.java
+++ manejo-salas/src/main/java/com/example/manejosalas/controlador/UsuarioControlador.java
@@ -1,12 +1,17 @@
 package com.example.manejosalas.controlador;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +27,8 @@ public class UsuarioControlador {
 	UsuarioDAO usuarioDAO;		
 
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("name", "Santiago");
 		return "index";
 	}	
 	
@@ -38,14 +44,95 @@ public class UsuarioControlador {
 	@GetMapping("/login")
 	public Usuario login(@RequestBody Usuario usuario) throws Exception {
 		UsuarioValidaciones validacion = new UsuarioValidaciones(usuario);
-		return validacion.getUser();
+		return validacion.loginGetUser();
 		
 	}
 	
-	@PostMapping("/register")
-	public void register(@RequestBody Usuario usuario) {		
-		usuarioDAO.save(usuario);
+	@GetMapping("/edit-usuario/{correo}")
+	public String getEditUsuarioForm(Model model, @PathVariable(name = "correo") String correo) throws Exception {
+		Usuario usuarioEdit = usuarioDAO.findByCorreo(correo);
+		
+		model.addAttribute("userForm", usuarioEdit);
+		model.addAttribute("userList", usuarioDAO.findAll());
+		model.addAttribute("perfiles",Perfil.getPerfiles());		
+		model.addAttribute("formTab","active");
+		
+		model.addAttribute("editMode", "active");
+		
+		return "mostrar-todos";
+				
+	}
+		
+	@GetMapping("/form/cancel")
+	public String cancelEditUser(ModelMap model) {
+		return "redirect:/usuarios/register";
 	}	
+	
+	@GetMapping("/delete/{id}") 
+	public String delete(Model model, @PathVariable(name = "id") int id){
+		try {
+			Optional<Usuario> usuario = usuarioDAO.findById(id);
+			usuarioDAO.delete(usuario.get());
+		}
+		catch(Exception e) {
+			model.addAttribute("listErrorMessage", e.getMessage());
+		}
+		
+		return register(model);
+	}
+	
+	@PostMapping("/register")
+	public String register(@ModelAttribute("userForm")Usuario usuario, BindingResult result, ModelMap model) throws Exception {
+		model.addAttribute("userList", usuarioDAO.findAll());
+		model.addAttribute("perfiles",Perfil.getPerfiles());	
+		
+		UsuarioValidaciones validacion = new UsuarioValidaciones(usuario);
+		
+		try{
+			validacion.registerGetUser();
+			
+			usuarioDAO.save(usuario);
+			
+			model.addAttribute("userForm", new Usuario());
+			model.addAttribute("formTab","active");
+		}		
+		catch(Exception e) {
+			model.addAttribute("formErrorMessage", e.getMessage());			
+			model.addAttribute("formTab","active");
+		}		
+		model.addAttribute("userForm", new Usuario());
+		model.addAttribute("userList", usuarioDAO.findAll());
+		model.addAttribute("perfiles",Perfil.getPerfiles());			
+		return "mostrar-todos";
+	}	
+	
+	@PostMapping("/edit")
+	public String edit(@ModelAttribute("userForm")Usuario usuario, BindingResult result, ModelMap model){
+		
+		model.addAttribute("userList", usuarioDAO.findAll());
+		model.addAttribute("perfiles",Perfil.getPerfiles());	
+		
+		UsuarioValidaciones validacion = new UsuarioValidaciones(usuario);
+		
+		Usuario registrado = validacion.getUsuarioRegistrado();
+		try{
+			validacion.mapUsuario(usuario, registrado);
+			usuarioDAO.save(registrado);			
+			model.addAttribute("userForm", new Usuario());
+			model.addAttribute("formTab","active");
+		}		
+		catch(Exception e) {
+			model.addAttribute("formErrorMessage", e.getMessage());			
+			model.addAttribute("formTab","active");
+			model.addAttribute("editMode", "true"); //Para que no cambie la pantalla y 
+													//podamos seguir editando
+		}	
+		
+		model.addAttribute("userForm", new Usuario());
+		model.addAttribute("userList", usuarioDAO.findAll());
+		model.addAttribute("perfiles",Perfil.getPerfiles());			
+		return "mostrar-todos";		
+	}
 	
 	@DeleteMapping
 	public void delete(@RequestBody Usuario usuario) {
@@ -70,16 +157,24 @@ public class UsuarioControlador {
 		return "index";
 	}
 	
-	
-	
+
 	public class UsuarioValidaciones{
 		
 		public final Usuario usuarioRegistrado;
+
 		public final Usuario usuarioPrueba;
 		public UsuarioValidaciones(Usuario usuario) {
 			
 			usuarioRegistrado = usuarioDAO.findByCorreo(usuario.getCorreo());
 			usuarioPrueba = usuario;
+		}
+		
+		public Usuario getUsuarioRegistrado() {
+			return usuarioRegistrado;
+		}
+
+		public Usuario getUsuarioPrueba() {
+			return usuarioPrueba;
 		}
 		
 		private boolean validarCredenciales() {
@@ -92,7 +187,7 @@ public class UsuarioControlador {
 			
 		}
 		
-		public Usuario getUser() throws Exception {
+		public Usuario loginGetUser() throws Exception {
 			
 			if(this.usuarioRegistrado == null) {
 				throw new Exception("El usuario no existe");
@@ -104,6 +199,31 @@ public class UsuarioControlador {
 				return this.usuarioRegistrado;
 			}
 		}
+		
+		public boolean registerGetUser() throws Exception{
+			
+			if(this.usuarioRegistrado == null) {
+				return true;
+			}
+			else {
+				throw new Exception("El usuario ya existe");
+			}
+		}
+		
+		/**
+		 * Map everythin but the password.
+		 * @param from
+		 * @param to
+		 */
+		protected void mapUsuario(Usuario from,Usuario to) {
+			to.setNombre(from.getNombre());
+			to.setApellido(from.getApellido());
+			to.setPerfil(from.getPerfil());
+			to.setCorreo(from.getCorreo());
+			//PODRIAMOS INCLUIR MÁS CAMPOS A MAPEAR
+		}
+
+		
 	}
 
 }
--- manejo-salas/src/main/java/com/example/manejosalas/entidad/Usuario.java
+++ manejo-salas/src/main/java/com/example/manejosalas/entidad/Usuario.java
@@ -12,6 +12,7 @@ import javax.persistence.Table;
 public class Usuario implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
     private int id;
 	
