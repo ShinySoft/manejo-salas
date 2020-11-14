7b299aef910eb14c14aa13264ae6f6b5f9ac2799
Martin <mgomezu@unal.edu.co>
2020-11-14 16:31:35 -0500

    Bugs solucionados

--- manejo-salas/src/main/java/com/example/manejosalas/DAO/UsuarioDAO.java
+++ manejo-salas/src/main/java/com/example/manejosalas/DAO/UsuarioDAO.java
@@ -2,11 +2,12 @@ package com.example.manejosalas.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import com.example.manejosalas.entidad.Usuario;
 
 @Repository
 public interface UsuarioDAO extends JpaRepository<Usuario, Integer> {
-
 	Usuario findByCorreo(String correo);
+	public List<Usuario> findAllByEstado(boolean estado);
+	
 }
